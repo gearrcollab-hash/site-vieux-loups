5bbdfe688d885a193f9391523199dd88b15e406f
gearrcollab-hash <gearr.collab@gmail.com>
2026-07-23 00:43:04 +0200

    🎬 Mise à jour des vidéos via le bot

--- videos.js
+++ videos.js
@@ -7,7 +7,7 @@
 const VIDEOS = [
   { url: "https://www.tiktok.com/@atomiixofficiel/video/7665245777509322006", image: "https://cdn.discordapp.com/attachments/1529547954069704824/1529556671939084478/image.png?ex=6a625e28&is=6a610ca8&hm=5f355b48c282bfeeca48814dd4674bb0176800b1355110f980b99d1f90c6c8e2&", titre: "🎬 Dernière opération", desc: "La meute en action sur FratWorld." },
   { url: "https://www.tiktok.com/@atomiixofficiel/video/7665047879274286358?is_from_webapp=1&sender_device=pc&web_id=7663499330703869462", image: "https://cdn.discordapp.com/attachments/1529547954069704824/1529619620208578771/image.png?ex=6a6298c8&is=6a614748&hm=7e6753d3b438fb5e2cd93d634a7d730d7aeb5dc75fd04ced9690b5aa632ca9aa&", titre: "🦽 Best-of de la meute", desc: "Les moments cultes des Vieux Loups." },
-  { url: "", image: "", titre: "⚔️ Guerre de territoire", desc: "Quand l'EHPAD montre les crocs." }
+  { url: "https://www.tiktok.com/@atomiixofficiel/video/7664951145546222870?is_from_webapp=1&sender_device=pc&web_id=7663499330703869462", image: "https://cdn.discordapp.com/attachments/1529547954069704824/1529619816535556136/image.png?ex=6a6298f7&is=6a614777&hm=3d06aff033a1ea04aaa27a8f2970d3b4802fab4577a7ddbadde43a3d3c769653&", titre: "⚔️ Guerre de territoire", desc: "Quand l'EHPAD montre les crocs." }
 ];
 
 if (typeof module !== "undefined") module.exports = VIDEOS;
