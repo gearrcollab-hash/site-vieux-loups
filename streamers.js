3b36834b3e57b04f5819ebf0b83bbd774237807e
gearrcollab-hash <gearr.collab@gmail.com>
2026-07-23 00:18:55 +0200

    Update streamers.js

--- streamers.js
+++ streamers.js
@@ -5,10 +5,26 @@
 
 const STREAMERS = [
   {
-    nom: "Test gear",
-    description: "test",
-    photo: "",
-    tiktok: "https://www.tiktok.com/@atomiixofficiel?is_from_webapp=1&sender_device=pc",
+    nom: "John Chibe",
+    description: "Fondateur",
+    photo: "https://cdn.discordapp.com/attachments/1529547954069704824/1529611912566935552/image.png?ex=6a62919a&is=6a61401a&hm=9610796f7e12ef898810c88936a3b15abc993af4e16b328200650c8e7b4addcf&",
+    tiktok: "https://www.tiktok.com/@john.chibe?is_from_webapp=1&sender_device=pc",
+    twitch: "https://www.twitch.tv/john_chibe",
+    kick: ""
+  },
+  {
+    nom: "Bows3R",
+    description: "Gros Streamer",
+    photo: "https://cdn.discordapp.com/attachments/1529547954069704824/1529613397740617781/image.png?ex=6a6292fc&is=6a61417c&hm=aa0909da1c058040242752b935cda71ca099e986cbe774a28126f8bcbd7c7fd9&",
+    tiktok: "https://www.tiktok.com/@bows3r13",
+    twitch: "https://www.twitch.tv/bows3r_fr",
+    kick: ""
+  },
+  {
+    nom: "Atomiix",
+    description: "Papi le Chef",
+    photo: "https://cdn.discordapp.com/attachments/1529547954069704824/1529613263120105472/image.png?ex=6a6292dc&is=6a61415c&hm=9ffee2a34ce80aea4235ab62fdaf8bc210dad657bf2cdbbf31bf32ea2b77a4e8&",
+    tiktok: "https://www.tiktok.com/@atomiixofficiel",
     twitch: "",
     kick: ""
   }
