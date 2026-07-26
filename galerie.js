fb86a2abbe6976862ebbda4d303af63bf42f9fe6
gearrcollab-hash <gearr.collab@gmail.com>
2026-07-26 02:28:20 +0200

    🖼️ Nouvelle photo dans la galerie via le bot

--- galerie.js
+++ galerie.js
@@ -5,6 +5,7 @@
 // ═══════════════════════════════════════════════════════════
 
 const GALERIE = [
+  { url: "https://cdn.discordapp.com/attachments/1529547797966360886/1530733446391926946/image.png?ex=6a66a61d&is=6a65549d&hm=beb483160dc7b2cd69d8c2685fbc9b9a9acf52864f5d9fdb84c58ee036ade5c0&", auteur: "gear_ontop", date: "2026-07-26T00:28:13.671Z", legende: "Quand tu trouves des groupes danser mdr" },
   { url: "https://cdn.discordapp.com/attachments/1529547797966360886/1530733415794737152/image.png?ex=6a66a615&is=6a655495&hm=d248c8967b1df152b5db27d242ed7ddd6b7e8faa3d5e35cb477b71ef0bff40d3&", auteur: "gear_ontop", date: "2026-07-26T00:28:06.246Z", legende: "" },
   { url: "https://cdn.discordapp.com/attachments/1529547797966360886/1530733154011185324/image.png?ex=6a66a5d7&is=6a655457&hm=bb87edabd84f5dd02cdb49ec504c54a18afdc3ecc963819db37e51515d9e98db&", auteur: "gear_ontop", date: "2026-07-26T00:27:04.031Z", legende: "" },
   { url: "https://cdn.discordapp.com/attachments/1529547797966360886/1530732347215970505/image.png?ex=6a66a517&is=6a655397&hm=e4154f744eeea9dd726434f36b4d74a0c83389566195a02b29620f98ad61687e&", auteur: "gear_ontop", date: "2026-07-26T00:23:51.642Z", legende: "" }
