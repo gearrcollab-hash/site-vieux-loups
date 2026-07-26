f9e51d12e9f00101e1b6e2b59ac6b859b549416e
gearrcollab-hash <gearr.collab@gmail.com>
2026-07-26 04:23:28 +0200

    🖼️ Nouvelle photo dans la galerie via le bot

--- galerie.js
+++ galerie.js
@@ -5,7 +5,18 @@
 // ═══════════════════════════════════════════════════════════
 
 const GALERIE = [
-
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530742397586116780/image.png?ex=6a66ae73&is=6a655cf3&hm=950bdc11bb5ed08643d0c5220d85a0a0b58732477b8c0b9683aaa21fe666eadf&","auteur":"gear_ontop","date":"2026-07-26T01:03:50.024Z","legende":"","album":"1530742397942628353"},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733558669246674/image.png?ex=6a66a638&is=6a6554b8&hm=bcc7d1ddf72eacb186a6e4ac7aa5621f2c7aba7451070b8aacf5d22f26f10bc7&","auteur":"gear_ontop","date":"2026-07-26T00:28:40.320Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733548066312252/image.png?ex=6a66a635&is=6a6554b5&hm=0d51b2482e1360f99d3c0602853baabf1023b74b31b9e3d28655da44fb040a25&","auteur":"gear_ontop","date":"2026-07-26T00:28:37.727Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733539664990288/image.png?ex=6a66a633&is=6a6554b3&hm=da645bac17d1e6339de142f6da5652fa7e0da5e23f38e74f2519abb33eec4840&","auteur":"gear_ontop","date":"2026-07-26T00:28:35.824Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733527853694986/image.png?ex=6a66a630&is=6a6554b0&hm=a534c63f0344d4405b949642f14565656b11a96c9760872c5afd0dfaef0b275c&","auteur":"gear_ontop","date":"2026-07-26T00:28:32.919Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733504524980385/image.png?ex=6a66a62b&is=6a6554ab&hm=57343bfc10451008c53f6a67ded54e69e8145a152273cf6bab1533921030b6fb&","auteur":"gear_ontop","date":"2026-07-26T00:28:27.431Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733489983586344/image.png?ex=6a66a627&is=6a6554a7&hm=5de3f9903bc0d012bd9eee92988ce8a9fde8ef7812cb7770f3fbf2d745f63381&","auteur":"gear_ontop","date":"2026-07-26T00:28:23.869Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733475605385296/image.png?ex=6a66a624&is=6a6554a4&hm=a0591531c61fed8c3ee76d1fc9201ee12c7c29542ee2539779315f0697a686c6&","auteur":"gear_ontop","date":"2026-07-26T00:28:20.474Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733446391926946/image.png?ex=6a66a61d&is=6a65549d&hm=beb483160dc7b2cd69d8c2685fbc9b9a9acf52864f5d9fdb84c58ee036ade5c0&","auteur":"gear_ontop","date":"2026-07-26T00:28:13.671Z","legende":"Quand tu trouves des groupes danser mdr","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733415794737152/image.png?ex=6a66a615&is=6a655495&hm=d248c8967b1df152b5db27d242ed7ddd6b7e8faa3d5e35cb477b71ef0bff40d3&","auteur":"gear_ontop","date":"2026-07-26T00:28:06.246Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530733154011185324/image.png?ex=6a66a5d7&is=6a655457&hm=bb87edabd84f5dd02cdb49ec504c54a18afdc3ecc963819db37e51515d9e98db&","auteur":"gear_ontop","date":"2026-07-26T00:27:04.031Z","legende":"","album":null},
+  {"url":"https://cdn.discordapp.com/attachments/1529547797966360886/1530732347215970505/image.png?ex=6a66a517&is=6a655397&hm=e4154f744eeea9dd726434f36b4d74a0c83389566195a02b29620f98ad61687e&","auteur":"gear_ontop","date":"2026-07-26T00:23:51.642Z","legende":"","album":null}
 ];
 
 if (typeof module !== "undefined") module.exports = GALERIE;
