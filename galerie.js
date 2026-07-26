a7d39826ec37a800aede68e03296eab18a0eb2e5
gearrcollab-hash <gearr.collab@gmail.com>
2026-07-26 02:28:06 +0200

    🖼️ Nouvelle photo dans la galerie via le bot

--- galerie.js
+++ galerie.js
@@ -5,6 +5,7 @@
 // ═══════════════════════════════════════════════════════════
 
 const GALERIE = [
+  { url: "https://cdn.discordapp.com/attachments/1529547797966360886/1530733154011185324/image.png?ex=6a66a5d7&is=6a655457&hm=bb87edabd84f5dd02cdb49ec504c54a18afdc3ecc963819db37e51515d9e98db&", auteur: "gear_ontop", date: "2026-07-26T00:27:04.031Z", legende: "" },
   { url: "https://cdn.discordapp.com/attachments/1529547797966360886/1530732347215970505/image.png?ex=6a66a517&is=6a655397&hm=e4154f744eeea9dd726434f36b4d74a0c83389566195a02b29620f98ad61687e&", auteur: "gear_ontop", date: "2026-07-26T00:23:51.642Z", legende: "" }
 ];
 
